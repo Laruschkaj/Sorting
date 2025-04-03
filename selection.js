ba0286b581da246e44e68bfcbe0e76f48074c4f0
Laruschkaj <laruschkajoubert@gmail.com>
2025-04-03 12:48:41 -0400

    Updated Selection Sort Function

--- selection.js
+++ selection.js
@@ -1,3 +1,31 @@
-function selectionSort() {}
+// Selection Sort Function
+function selectionSort(arr) {
+    // Loop over the entire array (starting from the first element)
+    for (let i = 0; i < arr.length; i++) {
+        // Assume the current element (at index i) is the smallest
+        let minIndex = i;
+
+        // Compare the current element with every other element after it
+        for (let j = i + 1; j < arr.length; j++) {
+            // If we find an element smaller than the current smallest
+            if (arr[j] < arr[minIndex]) {
+                // Update minIndex to point to the new smallest element
+                minIndex = j;
+            }
+        }
+
+        // After finding the smallest element in the unsorted portion
+        // Swap it with the element at the current index (i)
+        if (minIndex !== i) {
+            // Swap arr[i] and arr[minIndex]
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    // Return the sorted array after all iterations
+    return arr;
+}
 
 module.exports = selectionSort;
